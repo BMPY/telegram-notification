0b876debb5419cb6a0d5cccc11ef63447ca64d41
shih <shih@shih>
2024-08-24 22:33:28 +0700

    init project

---
+++ src/main/java/com/telegram_notification_backend/TelegramNotificationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.telegram_notification_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramNotificationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramNotificationBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telegram_notification_backend/TelegramNotificationBackendApplicationTests.java
@@ -0,0 +1,8 @@
+package com.telegram_notification_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramNotificationBackendApplicationTests {
+}
